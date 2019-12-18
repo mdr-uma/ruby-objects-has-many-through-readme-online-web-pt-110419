be513cb80b681caa88eecaaf7a90adb538628ee7
Uma Manandhar <um_2003_8@hotmail.com>
2019-12-18 23:47:39 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,8 +1,15 @@
 class Waiter
   attr_accessor :name, :years_of_experience
 
+  @@all = []
+  
   def initialize(name, years_of_experience)
     @name = name
     @years_of_experience = years_of_experience
+    @@all <<self
+  end
+
+  def self.all
+    @@all
   end
 end
