43aaf1d5ee80cac87bd00d55bf64d761ec3d51ac
Uma Manandhar <um_2003_8@hotmail.com>
2019-12-19 01:16:27 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -27,7 +27,7 @@ class Waiter
     # Customer.all.find {|customer| customer.max.tip}
     best_tipped_meal = meals.max do |meal_a, meal_b|
     meal_a.tip <=> meal_b.tip
-    binding.pry
+    # binding.pry
   end
 
   best_tipped_meal.customer
