4d8ad4269077c66c2e0cb2ef64cb4cf93a2067c0
Uma Manandhar <um_2003_8@hotmail.com>
2019-12-19 00:05:58 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -15,7 +15,10 @@ class Customer
 
   def meals
     Meal.all.select {|meal| meal.customer == self}
+  end
 
+  def waiters
+    meals.map {|meal| meal.waiter}
   end
 
   def new_meal(waiter, total, tip)
